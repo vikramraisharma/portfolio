d6b6c6d038a2fb326152d6f3eb58c79aabc441e4
unknown <vikram.sharma1024@gmail.com>
2020-08-19 14:29:47 -0400

    hero text format and projects size

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,9 @@ class Hero extends Component {
             <div className="hero-div columns">
 
                         <div className="hero-text column is-half">
-                            <h1 className="subtitle is-4">Here's a brand statement or title. I plan on talking about myself a little and putting my resume brand statement here.</h1>
+                            <div className="bio">
+                                <h1 className="subtitle is-5">Here's a brand statement or title. I plan on talking about myself a little and putting my resume brand statement here.</h1>
+                            </div>
                         </div>
                         <div className="hero-img column">
                             img here
