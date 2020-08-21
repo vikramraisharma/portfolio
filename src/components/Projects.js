29459ac58015dcad9b5adaab84a09af836a0fe8b
unknown <vikram.sharma1024@gmail.com>
2020-08-20 22:27:49 -0400

    project anim

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,18 +5,18 @@ class Projects extends Component{
         return(
             <div className="projects tile">
                     <div className="tile is-parent is-vertical is-5">
-                        <article className="tile is-child notification is-warning">
+                        <article className="tile is-child notification is-warning hvr-grow" >
                             <p>lorem ipsum dolor sit amet</p>
                         </article>
-                        <article className="tile is-child notification is-warning">
+                        <article className="tile is-child notification is-warning hvr-grow">
                             <p>lorem ipsum dolor sit amet</p>
                         </article>
                     </div>
                     <div className="tile is-parent is-vertical is-5">
-                        <article className="tile is-child notification is-warning">
+                        <article className="tile is-child notification is-warning hvr-grow">
                             <p>lorem ipsum dolor sit amet</p>
                         </article>
-                        <article className="tile is-child notification is-warning">
+                        <article className="tile is-child notification is-warning hvr-grow">
                             <p>lorem ipsum dolor sit amet</p>
                         </article>
                     </div>
