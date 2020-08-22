211dbcf19109e3e415fb691990d22f3bc2645f83
Vikram <vikram.sharma1024@gmail.com>
2020-08-22 17:38:04 -0400

    content sections finished

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -5,16 +5,16 @@ class Blog extends Component {
         return(
             <div className="blog">
                     <div className="tile is-parent is-vertical is-10">
-                        <article className="tile is-child notification is-dark">
-                            <p class="title">Wide tile</p>
+                        <article className="tile is-child notification is-dark blog-post">
+                            <h1 class="title">Wide tile</h1>
                             <p class="subtitle">Aligned with the right tile</p>
                         </article>
-                        <article className="tile is-child notification is-dark">
-                            <p class="title">Wide tile</p>
+                        <article className="tile is-child notification is-dark blog-post">
+                            <h1 class="title">Wide tile</h1>
                             <p class="subtitle">Aligned with the right tile</p>
                         </article>
-                        <article className="tile is-child notification is-dark">
-                            <p class="title">Wide tile</p>
+                        <article className="tile is-child notification is-dark blog-post">
+                            <h1 class="title">Wide tile</h1>
                             <p class="subtitle">Aligned with the right tile</p>
                         </article>
                     </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import madewithbulma from '../images/made-with-bulma--white.png'
 
 class Footer extends Component {
     render() {
         return(
             <footer className="footer">
-
+                <div className="level">
+                    <div className="level-left">
+                        <div className="level-item">
+                            <a href="https://bulma.io">
+                                <img src={madewithbulma} alt="Made with Bulma" width="192" height="36"/>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="level-right">
+                        <div className="level-item">
+                            <a href="#"> <i class="fab fa-instagram" style={{fontSize: 2 + 'rem', color: 'white'}}/> </a>
+                        </div>
+                        <div className="level-item">
+                            <a href="#"> <i class="fab fa-github" style={{fontSize: 2 + 'rem', color: 'white'}}/> </a>
+                        </div>
+                        <div className="level-item">
+                            <a href="#"> <i class="fab fa-linkedin" style={{fontSize: 2 + 'rem', color: 'white'}}/> </a>
+                        </div>
+                    </div>
+                </div>
             </footer>
         )
     }
