150ab74bb3a5ecf96a746e88951fee8abe9d9b16
vksharma <vikram.sharma1024@gmail.com>
2020-10-21 22:47:55 -0400

    clearance review, fix margins, update projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Parallax} from 'react-scroll-parallax';
 
-import Sinatra from '../images/sinatra.png'
+import Plant from '../images/plantarion.png'
 import Stomping from '../images/stomping.png'
 import Tzo from '../images/tzo.png'
 import Dsiac from '../images/dsiac.png'
@@ -14,32 +14,30 @@ class Projects extends Component{
                 <div className="projects tile">
                         <div className="tile is-parent is-vertical is-7">
                             <article className="tile is-child notification hvr-grow">
-                                <a href="https://www.dsiac.org/">
+                                <a href="https://www.plantarion.com/">
                                     <figure className="image is-4by3">
-                                        <img src={ Dsiac } alt="project number 4 on display"/>
+                                        <img src={Plant} alt="plant-based service and food startup" />
                                     </figure>
                                 </a>
-                                <p>Defense Systems IAC -- Govt Contractor dedicated to military research efforts and report writing</p>
+                                <p>Plantarion -- Plant-based startup aimed at connecting PB service providers to consumers</p>
                             </article>
                             <article className="tile is-child notification hvr-grow">
                                 <a href="https://tzo-strength.herokuapp.com/">
                                     <figure className="image is-4by3">
-                                        <img src={ Tzo } alt="strength coaching web app"/>
+                                        <img src={Tzo} alt="strength coaching web app" />
                                     </figure>
                                 </a>
                                 <p>TZO Strength -- A commissioned web app to market strength training and powerlifting coach Abiy Ababu</p>
-                            </article>
-                            
+                            </article> 
                         </div>
                         <div className="tile is-parent is-vertical is-7">
-                             <article className="tile is-child notification hvr-grow">
-                                <a href="https://github.com/vikramraisharma/moon-river-api">
-                                    <figure className="project-img is-4by3">
-                                        <img src={ Sinatra } alt="music blog inspired by Sinatra himself"/>
+                            <article className="tile is-child notification hvr-grow">
+                                <a href="https://www.dsiac.org/">
+                                    <figure className="image is-4by3">
+                                        <img src={Dsiac} alt="project number 4 on display" />
                                     </figure>
                                 </a>
-                                <p>Moon River -- A music blog with a back-end built in Sinatra.rb, Sinatra is a lightweight Ruby framework used for back and front end development.
-                                </p>
+                                <p>Defense Systems IAC -- Govt Contractor dedicated to military research efforts and report writing</p>
                             </article>
                             <article className="tile is-child notification hvr-grow">
                                 <a href="https://enigmatic-mountain-68507.herokuapp.com/">
