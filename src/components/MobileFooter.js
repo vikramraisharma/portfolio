caa5395ef920f9fc5717226272ba03633b49972a
vikramraisharma <vikram.sharma1024@gmail.com>
2020-11-17 14:37:43 -0500

    Update MobileFooter.js
    update instagram link

--- src/components/MobileFooter.js
+++ src/components/MobileFooter.js
@@ -11,7 +11,7 @@ class Footer extends Component {
                     <div className="mobile-footer-links">
                             <ul className="mobile-icon-list">
                                 <li><a href="https://github.com/vikramraisharma"> <i className="fab fa-github" style={{fontSize: 3 + 'rem', color: 'var(--sky)'}} target="_blank" rel="noopener noreferrer"/> </a></li>
-                                <li><a href="https://www.instagram.com/thebusinessbeard/"> <i className="fab fa-instagram" style={{fontSize: 3 + 'rem', color: 'var(--sky)'}} target="_blank" rel="noopener noreferrer"/> </a></li>
+                                <li><a href="https://www.instagram.com/vikramraisharma/"> <i className="fab fa-instagram" style={{fontSize: 3 + 'rem', color: 'var(--sky)'}} target="_blank" rel="noopener noreferrer"/> </a></li>
                                 <li><a href="https://www.linkedin.com/in/vikramraisharma/"> <i className="fab fa-linkedin" style={{fontSize: 3 + 'rem', color: 'var(--sky)'}} target="_blank" rel="noopener noreferrer"/> </a></li>
                             </ul>
                     </div>
